e50894f69fd7437e3fa2ce8fe809c719c6f3200a
Sami Khalfi <wwwkhalfi2003@gmail.com>
2024-10-14 20:03:12 +0100

    minor changes

--- utils/news_scrap.py
+++ utils/news_scrap.py
@@ -1,39 +1,38 @@
 import requests
 import pandas as pd
 import time
-import os
 
-API_KEY = 'cs3g1hhr01qkg08j9hcgcs3g1hhr01qkg08j9hd0'  
+API_KEY = 'cs4p4u1r01qgd1p6742gcs4p4u1r01qgd1p67430'
 
 def fetch_company_news(symbol, from_date, to_date):
     url = f'https://finnhub.io/api/v1/company-news?symbol={symbol}&from={from_date}&to={to_date}&token={API_KEY}'
-    
+
     try:
         response = requests.get(url)
-        response.raise_for_status()  
+        response.raise_for_status()
 
         news_data = response.json()
         
-        if news_data:
-            news_df = pd.DataFrame(news_data)
-            file_path = 'Data/Company_News.csv'
-            news_df.to_csv(file_path, index=False)
-            print(f"Fetched {len(news_data)} news articles for {symbol} from {from_date} to {to_date}.")
-            return news_df
-        else:
-            print(f"No news found for {symbol} from {from_date} to {to_date}.")
-            return None
+        news_df = pd.DataFrame(news_data)
         
-    except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")
-        return None
+        if 'datetime' in news_df.columns:
+
+            news_df['date'] = pd.to_datetime(news_df['datetime'], unit='s').dt.date
+        
+        news_df = news_df[['headline', 'date', 'url']]
+
+        csv_filename = f'Data/Company_News.csv'
+        news_df.to_csv(csv_filename, index=False)
+
+        print(f"Fetched {len(news_data)} news articles for {symbol} from {from_date} to {to_date}.")
+        return news_df
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"Error fetching data: {e}")
         return None
-    
+
 if __name__ == "__main__":
-    symbol = 'AAPL' 
-    from_date = '2023-08-15'  
+    symbol = 'AAPL'
+    from_date = '2023-08-15'
     to_date = '2024-08-20'
 
     fetch_company_news(symbol, from_date, to_date)
